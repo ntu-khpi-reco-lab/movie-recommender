a7acde29008e699628c84b42705900bb5c30daae
MykytaKa <g0dn1k142@gmail.com>
2024-10-17 17:18:34 +0300

    Completed code improving: 1. Renamed the db and added new columns; 2. Added logging; 3. Integrated DTO; 4. Integrated ControllerAdvice exception handler. 5. Reorganized the structure of the service folders.

--- location-service/src/main/java/com/movie/recommender/location/LocationServiceApplication.java
+++ location-service/src/main/java/com/movie/recommender/location/LocationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.movie.recommender.location_service;
+package com.movie.recommender.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- location-service/src/main/java/com/movie/recommender/location_service/LocationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.movie.recommender.location_service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
