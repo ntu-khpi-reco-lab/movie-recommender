363c39a4fbb778597aee0805d6bf044921e2c40e
ValeZh <mugikaoven@gmail.com>
2024-10-22 14:10:28 +0300

    commit changes

--- crawler/src/main/java/com/movie/recommender/crawler/Main.java
+++ crawler/src/main/java/com/movie/recommender/crawler/Main.java
@@ -11,18 +11,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Films films = new Films(1);
         System.out.println(films.ongoing_film_results_array.size());
+        List<String> film_names = films.getFilmNames(films.ongoing_film_results_array);
         System.out.println(films.getFilmNames(films.ongoing_film_results_array).size());
 
         String language = "en";
         String search_region = "us";
         SerpApiShowtimes showtimes = new SerpApiShowtimes(language, search_region);
 
-        List<String> film_names = new ArrayList<>();
+        List<String> film_names_for_db = new ArrayList<>();
         List<String> locations = new ArrayList<>();
-        film_names.add("Joker 2");
+        film_names_for_db.add(film_names.get(0));
         locations.add("Austin, Texas, United States");
 
-        //JsonArray showtimes_db_filler = showtimes.makeDBFiller(film_names, locations);
+        //JsonArray showtimes_db_filler = showtimes.makeDBFiller(film_names_for_db, locations);
 
         CsvWorker csv = new CsvWorker();
         csv.csvLoader("crawler/src/main/java/resources/tbd_data.csv", films.film_results_array.toString());
--- crawler/src/main/java/com/movie/recommender/crawler/SerpApiShowtimes.java
+++ crawler/src/main/java/com/movie/recommender/crawler/SerpApiShowtimes.java
@@ -6,8 +6,6 @@ import com.hw.serpapi.SerpApiSearchException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.ZoneId;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
