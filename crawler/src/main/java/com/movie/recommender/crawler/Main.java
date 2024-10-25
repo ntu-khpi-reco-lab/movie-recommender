f1872fb48e9cc6765f1666d53215a52f8deff590
ValeZh <mugikaoven@gmail.com>
2024-10-25 20:19:33 +0300

    Some pretify

--- crawler/src/main/java/com/movie/recommender/crawler/Main.java
+++ crawler/src/main/java/com/movie/recommender/crawler/Main.java
@@ -7,8 +7,5 @@ import com.movie.recommender.crawler.client.UrlBuilder;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Crawler started");
-
-        SerpApiClient client = new SerpApiClient();
-        System.out.println( client.getMovieShowtimes("The wild robot","Austin, Texas, United States","en"));
     }
 }
