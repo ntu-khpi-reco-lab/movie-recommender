f67bc860204f4adcefd060cdfc3b614fdc3670ab
MykytaKa <g0dn1k142@gmail.com>
2024-10-31 21:45:26 +0200

    Delete unnecessary comment

--- location-service/src/main/java/com/movie/recommender/location/LocationServiceApplication.java
+++ location-service/src/main/java/com/movie/recommender/location/LocationServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-// https://geocode.xyz/{lat},{long}?geoit=json - API для отримання більш точної геолокації
 @SpringBootApplication
 public class LocationServiceApplication {
     public static void main(String[] args) {
