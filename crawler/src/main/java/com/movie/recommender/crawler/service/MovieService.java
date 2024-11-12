dc165c87084cd75afc60d5de5884df7cd48de6a4
ValeZh <mugikaoven@gmail.com>
2024-11-12 23:19:05 +0200

    resolve conversations

--- crawler/src/main/java/com/movie/recommender/crawler/service/MovieService.java
+++ crawler/src/main/java/com/movie/recommender/crawler/service/MovieService.java
@@ -1,11 +1,8 @@
 package com.movie.recommender.crawler.service;
 
 import com.movie.recommender.common.client.LocationServiceClient;
-
 import com.movie.recommender.common.model.location.CountryWithCitiesDTO;
-import com.movie.recommender.common.model.movie.MovieCredits;
 import com.movie.recommender.common.model.movie.MovieDetails;
-import com.movie.recommender.common.model.movie.MovieKeywords;
 import com.movie.recommender.common.model.movie.NowPlayingMoviesByCountry;
 import com.movie.recommender.common.model.showtime.ShowtimesByCity;
 import com.movie.recommender.crawler.client.SerpApiClient;
@@ -47,30 +44,27 @@ public class MovieService {
             log.info("Processing country '{}'", countryName);
 
             NowPlayingMoviesByCountry movies = fetchNowPlayingMoviesForCountry(countryCode);
-            processCountryShowtimes(country, movies);
-
+            if (movies != null) {
+                processCountryShowtimes(country, countryCode, movies);
+            }
         }
     }
-
 
     private NowPlayingMoviesByCountry fetchNowPlayingMoviesForCountry(String countryCode) {
-
-
         log.info("Loading now playing movies for country '{}'", countryCode);
 
-        Optional<NowPlayingMoviesByCountry> nowPlayingMovie = tmdbApiClient.getNowPlayingMovies(countryCode);
-        if (nowPlayingMovie.isPresent()) {
-            nowPlayingMovie.get().setCountryCode(countryCode);
-            mongoDBService.insertNowPlayingMovies(nowPlayingMovie.get());
-            processNowPlayingMoviesDetails(nowPlayingMovie.get());
+        Optional<NowPlayingMoviesByCountry> nowPlayingMovieOpt = tmdbApiClient.getNowPlayingMovies(countryCode);
+        if (nowPlayingMovieOpt.isPresent()) {
+            NowPlayingMoviesByCountry nowPlayingMovie = nowPlayingMovieOpt.get();
+            nowPlayingMovie.setCountryCode(countryCode);
+            processNowPlayingMoviesDetails(nowPlayingMovie);
             log.info("Now playing movies for country '{}' inserted into MongoDB.", countryCode);
+            return nowPlayingMovie;
         } else {
             log.warn("No now playing movies found for country '{}'.", countryCode);
+            return null;
         }
-
-        return nowPlayingMovie.get();
     }
-
 
     public void processNowPlayingMoviesDetails(NowPlayingMoviesByCountry nowPlayingMovie) {
         List<MovieDetails> movieDetailsList = new ArrayList<>();
@@ -92,7 +86,6 @@ public class MovieService {
 
         log.info("Processing Movie ID: {}", movieId);
 
-        // Load movie details
         Optional<MovieDetails> movieDetailsOpt = tmdbApiClient.getMovieDetails(movieId.toString());
         if (movieDetailsOpt.isEmpty()) {
             log.warn("No details found for movie ID: {}", movieId);
@@ -101,22 +94,17 @@ public class MovieService {
 
         MovieDetails movieDetails = movieDetailsOpt.get();
 
-        Optional<MovieCredits> creditsOpt = tmdbApiClient.getMovieCredits(movieId.toString());
-        creditsOpt.ifPresent(credits -> {
+        tmdbApiClient.getMovieCredits(movieId.toString()).ifPresent(credits -> {
             movieDetails.setCast(credits.getCast());
             movieDetails.setCrew(credits.getCrew());
         });
 
-        Optional<MovieKeywords> keywordsOpt = tmdbApiClient.getMovieKeywords(movieId.toString());
-        keywordsOpt.ifPresent(keywords -> movieDetails.setKeywords(keywords.getKeywords()));
+        tmdbApiClient.getMovieKeywords(movieId.toString()).ifPresent(keywords -> movieDetails.setKeywords(keywords.getKeywords()));
 
         return Optional.of(movieDetails);
     }
 
-    private void processCountryShowtimes(CountryWithCitiesDTO country, NowPlayingMoviesByCountry nowPlayingMoviesByCountry) {
-        String countryCode = nowPlayingMoviesByCountry.getCountryCode();
-        String countryName = country.getCountryName();
-
+    private void processCountryShowtimes(CountryWithCitiesDTO country, String countryCode, NowPlayingMoviesByCountry nowPlayingMovies) {
         log.info("Loading showtimes for country '{}'", countryCode);
 
         for (String cityName : country.getCities()) {
@@ -124,24 +112,33 @@ public class MovieService {
             showtimesByCity.setCountryCode(countryCode);
             showtimesByCity.setCityName(cityName);
 
-            List<ShowtimesByCity.MovieShowtimes> movieList = processMovies(nowPlayingMoviesByCountry.getResults(), cityName,countryName);
+            List<ShowtimesByCity.MovieShowtimes> movieList = processMovies(
+                    nowPlayingMovies.getResults(),
+                    cityName,
+                    country.getCountryName()
+            );
 
             showtimesByCity.setMovies(movieList);
             mongoDBService.insertShowtimes(showtimesByCity);
         }
-
     }
 
-    private List<ShowtimesByCity.MovieShowtimes> processMovies(List<NowPlayingMoviesByCountry.MovieIdentifier> movies, String cityName, String country) {
+    private List<ShowtimesByCity.MovieShowtimes> processMovies(
+            List<NowPlayingMoviesByCountry.MovieIdentifier> movies,
+            String cityName,
+            String country
+    ) {
         List<ShowtimesByCity.MovieShowtimes> movieList = new ArrayList<>();
         int apiCallCount = 0;
+
         for (NowPlayingMoviesByCountry.MovieIdentifier movie : movies) {
             if (apiCallCount >= API_CALL_LIMIT) {
                 log.info("Reached the limit of {} API calls. Exiting movie loop.", API_CALL_LIMIT);
                 break;
             }
-            String cityWithCountryName =country + "," + cityName ;
-            ShowtimesByCity.MovieShowtimes showtimeMovie = fetchMovieShowtimes(movie, cityWithCountryName);
+
+            String location = country + ", " + cityName;
+            ShowtimesByCity.MovieShowtimes showtimeMovie = fetchMovieShowtimes(movie, location);
 
             if (showtimeMovie != null) {
                 movieList.add(showtimeMovie);
@@ -151,12 +148,12 @@ public class MovieService {
         return movieList;
     }
 
-    private ShowtimesByCity.MovieShowtimes fetchMovieShowtimes(NowPlayingMoviesByCountry.MovieIdentifier movie, String cityName) {
+    private ShowtimesByCity.MovieShowtimes fetchMovieShowtimes(NowPlayingMoviesByCountry.MovieIdentifier movie, String location) {
         ShowtimesByCity.MovieShowtimes showtimeMovie = new ShowtimesByCity.MovieShowtimes();
         showtimeMovie.setId(movie.getId());
         showtimeMovie.setTitle(movie.getTitle());
 
-        Optional<MovieShowtimesResponse> showtimesResponse = serpApiClient.getMovieShowtimes(movie.getTitle(), cityName, "en");
+        Optional<MovieShowtimesResponse> showtimesResponse = serpApiClient.getMovieShowtimes(movie.getTitle(), location, "en");
 
         if (showtimesResponse.isPresent()) {
             showtimeMovie.setShowtimes(showtimesResponse.get().getShowtimes());
