1327f9c50e23e9be3d95d144a0a4cd910d938299
ValeZh <mugikaoven@gmail.com>
2024-11-15 16:37:40 +0200

    I make endpoint and test it

--- crawler/src/main/java/com/movie/recommender/crawler/controller/CrawlerController.java
+++ crawler/src/main/java/com/movie/recommender/crawler/controller/CrawlerController.java
@@ -1,4 +1,32 @@
 package com.movie.recommender.crawler.controller;
 
+import com.movie.recommender.crawler.service.MovieService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/internal")
+@Slf4j
 public class CrawlerController {
+
+    private final MovieService movieService;
+
+    public CrawlerController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping("/crawl")
+    public ResponseEntity<String> triggerCrawl() {
+        try {
+            log.info("Manual crawl process initiated.");
+            movieService.processCountryMovies();
+            log.info("Crawl process completed successfully.");
+            return ResponseEntity.ok("Crawl process completed successfully.");
+        } catch (Exception e) {
+            log.error("Error during crawl process: {}", e.getMessage(), e);
+            return ResponseEntity.internalServerError().body("An error occurred during the crawl process.");
+        }
+    }
 }
