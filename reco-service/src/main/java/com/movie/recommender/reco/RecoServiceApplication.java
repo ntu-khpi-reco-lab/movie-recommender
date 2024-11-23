8b55858c022c3d4497bf8480d93c2b90414fb839
ValeZh <mugikaoven@gmail.com>
2024-11-24 01:11:17 +0200

    start making responces

--- reco-service/src/main/java/com/movie/recommender/reco/RecoServiceApplication.java
+++ reco-service/src/main/java/com/movie/recommender/reco/RecoServiceApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+        exclude = {DataSourceAutoConfiguration.class},
+        scanBasePackages = {
+                "com.movie.recommender.common.client",
+                "com.movie.recommender.reco"
+        }
+)
 @EnableFeignClients(basePackages = {
         "com.movie.recommender.common.client"
 })
--- reco-service/src/main/java/com/movie/recommender/reco/service/RecommendationService.java
+++ reco-service/src/main/java/com/movie/recommender/reco/service/RecommendationService.java
@@ -2,48 +2,70 @@ package com.movie.recommender.reco.service;
 
 import com.movie.recommender.common.client.FavoritesClient;
 import com.movie.recommender.common.client.LocationServiceClient;
+import com.movie.recommender.common.client.MovieRecoClient;
 import com.movie.recommender.common.model.location.LocationDTO;
 import com.movie.recommender.common.model.movie.NowPlayingMoviesByCountry;
+import com.movie.recommender.common.model.reco.PredictRequest;
+import com.movie.recommender.common.model.reco.PredictResponse;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.HashMap;
-
+@Slf4j
 @Service
 public class RecommendationService {
 
     private final LocationServiceClient locationClient;
     private final FavoritesClient favoritesClient;
     private final MongoDBService mongoDBService;
+    private final MovieRecoClient movieRecoClient;
     private final String COUNTRY_CODE = "ua";
 
     public RecommendationService(
             LocationServiceClient locationClient,
-            FavoritesClient favoritesClient,MongoDBService mongoDBService
+            FavoritesClient favoritesClient,
+            MongoDBService mongoDBService,
+            MovieRecoClient movieRecoClient
     ) {
         this.locationClient = locationClient;
         this.favoritesClient = favoritesClient;
         this.mongoDBService = mongoDBService;
-
+        this.movieRecoClient = movieRecoClient;
     }
 
-    // Основной метод для получения рекомендаций
+
+
     public Map<String, Object> getRecommendations(Long userId) {
+        log.info("Fetching location for user ID: {}", userId);
         LocationDTO location = locationClient.getLocationByUserId(userId);
 
+        log.info("Fetching favorite movies for user ID: {}", userId);
         Set<Long> favoriteMovies = favoritesClient.getFavoriteMovies(userId);
 
+        log.info("Fetching now playing movies for country code: {}", COUNTRY_CODE);
         List<NowPlayingMoviesByCountry> nowPlayingMovies = mongoDBService.getNowPlayingMoviesByCountry(COUNTRY_CODE);
 
-        // Формируем ответ
+        List<Long> nowPlayingMovieIds = nowPlayingMovies.stream()
+                .flatMap(country -> country.getResults().stream())
+                .map(NowPlayingMoviesByCountry.MovieIdentifier::getId)
+                .collect(Collectors.toList());
+        log.info("Now playing movie IDs: {}", nowPlayingMovieIds);
+
+        PredictRequest predictRequest = new PredictRequest();
+        predictRequest.setLikedMovieIds(new ArrayList<>(favoriteMovies));
+        predictRequest.setMovieIds(nowPlayingMovieIds);
+        log.info("Sending predict request: {}", predictRequest);
+
+        //PredictResponse predictResponse = movieRecoClient.getRecommendations(predictRequest);
+        //log.info("Received predict response: {}", predictResponse);
+
         Map<String, Object> response = new HashMap<>();
         response.put("userId", userId);
+        response.put("location", location);
         response.put("favorites", favoriteMovies);
-        response.put("nowPlaying", nowPlayingMovies);
+        //response.put("recommendations", predictResponse);
 
         return response;
     }
-
 }
