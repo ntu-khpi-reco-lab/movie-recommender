74bec38b90efc1705a452604dbad38419f7f7e57
MykytaKa <g0dn1k142@gmail.com>
2024-12-03 20:09:45 +0200

    Added JsonCreator to LocationMessage's constructor

--- common/src/main/java/com/movie/recommender/common/model/location/LocationMessage.java
+++ common/src/main/java/com/movie/recommender/common/model/location/LocationMessage.java
@@ -1,14 +1,18 @@
 package com.movie.recommender.common.model.location;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import java.io.Serializable;
 
 @Data
-public class LocationMessage implements Serializable {
+public class LocationMessage {
     private String countryCode;
     private String cityName;
 
-    public LocationMessage(String countryCode, String cityName) {
+    @JsonCreator
+    public LocationMessage(
+            @JsonProperty("countryCode") String countryCode,
+            @JsonProperty("cityName") String cityName) {
         this.countryCode = countryCode;
         this.cityName = cityName;
     }
