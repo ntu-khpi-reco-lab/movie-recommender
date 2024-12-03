417f9e5060e52c4d8b8de3a2b61d41ad28095e87
Alexandra <atamanenkoa@school73kharkov.com>
2024-12-03 02:17:59 +0200

    changes in the PredictResponse. replacing the endpoint with /predict by /recommend. delete mockResponse fnc

--- common/src/main/java/com/movie/recommender/common/client/MovieRecoClient.java
+++ common/src/main/java/com/movie/recommender/common/client/MovieRecoClient.java
@@ -2,13 +2,10 @@ package com.movie.recommender.common.client;
 
 import com.movie.recommender.common.model.reco.PredictRequest;
 import com.movie.recommender.common.model.reco.PredictResponse;
-import com.movie.recommender.common.model.reco.Prediction;
 import com.movie.recommender.lib.http.HttpClient;
 import com.movie.recommender.lib.http.auth.NoAuthProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 
 @Slf4j
@@ -23,15 +20,10 @@ public class MovieRecoClient {
     }
 
     public PredictResponse getRecommendations(PredictRequest request) {
-        String url = baseUrl + "/predict";
+        String url = baseUrl + "/recommend";
         log.info("Sending POST request to {}", url);
 
-        // Real call to the MovieReco service
-        // PredictResponse predictResponse = httpClient.post(url, request, PredictResponse.class);
-
-        // For testing purposes
-        // Simulate the response from the MovieReco service by generating random predictions
-        PredictResponse predictResponse = mockResponse(request);
+         PredictResponse predictResponse = httpClient.post(url, request, PredictResponse.class);
 
         log.info("Received response from MovieReco service: {}", predictResponse);
         return predictResponse;
@@ -44,20 +36,5 @@ public class MovieRecoClient {
             throw new IllegalStateException("MOVIE_RECO_BASE_URL environment variable is not set");
         }
         return baseUrl;
-    }
-
-    // For testing purposes
-    // Simulate the response from the MovieReco service by generating random predictions
-    // Will be deleted in the final version
-    private PredictResponse mockResponse(PredictRequest request) {
-        PredictResponse response = new PredictResponse();
-        // Ensure the list is initialized
-        if (response.getPredictions() == null) {
-            response.setPredictions(new ArrayList<>());
-        }
-        for (Long movieId : request.getMovieIds()) {
-            response.getPredictions().add(new Prediction(movieId, Math.random()));
-        }
-        return response;
     }
 }
